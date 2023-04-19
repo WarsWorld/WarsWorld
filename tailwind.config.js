12f75119563831ce62b840fcce7ba431b97c7b15
Wipple <krugthebearking@gmail.com>
2023-04-19 15:32:38 -0400

    setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: '@',
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      'sans': ['Proxima Nova', ...defaultTheme.fontFamily.sans],
+      colors:
+      {
+        'primary': '#E47220',
+        'primary-variant': '#9EBC9F'
+      }
+    },
+  },
+  plugins: [],
+}
+
