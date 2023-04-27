63dd30980dd11fb841f3eac7fe96ae00db9c3db4
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2023-04-26 20:58:17 -0400

    styling leaderboard

--- src/pages/league/index.tsx
+++ src/pages/league/index.tsx
@@ -5,22 +5,22 @@ interface LeaderBoardProps {
 
 function LeaderBoard({ title, players }: LeaderBoardProps) {
   return (
-    <div className="@w-80 @border-blue-900 @border-4">
-      <div>{title}</div>
-      <table>
+    <div className="@w-80 @bg-black/70 @rounded-md @py-2">
+      <div className="@w-full @text-center">{title}</div>
+      <table className="@w-full @text-left">
         <thead>
           <tr>
-            <th>#</th>
-            <th>Player</th>
-            <th>Rating</th>
+            <th className="@px-5 @py-2">#</th>
+            <th className="@px-5 @py-2">Player</th>
+            <th className="@px-5 @py-2">Rating</th>
           </tr>
         </thead>
         <tbody>
           {players.map((player, idx) => (
             <tr key={idx}>
-              <td>{idx + 1}</td>
-              <td>{player}</td>
-              <td>{(idx + 1) * 100}</td>
+              <td className="@px-5 @py-1">{idx + 1}</td>
+              <td className="@px-5 @py-1">{player}</td>
+              <td className="@px-5 @py-1">{(idx + 1) * 100}</td>
             </tr>
           ))}
         </tbody>
@@ -42,10 +42,10 @@ export default function LeaguePage() {
 
   return (
     <div className="@h-screen @w-full @flex @justify-center @items-center">
-      <div className="@flex @justify-center @items-center @flex-col @max-w-6xl @h-full @border-red-900 @border-4">
+      <div className="@flex @justify-center @items-center @flex-col @max-w-7xl @h-full @border-red-900 @border-4">
         <div>Wars World Global League</div>
 
-        <div className=" @border-red-900 @border-4">
+        <div>
           <h3>
             The Global League is the premier AWBW competition for players of all
             skill levels:
@@ -53,7 +53,7 @@ export default function LeaguePage() {
 
           <ul className="@list-disc @ml-8">
             <li>Ranked asynchronous 1 vs. 1 matches</li>
-            <li>3 game modes: standard, fog, high funds</li>
+            <li>Game mode: Standard</li>
             <li>Automatic game creation</li>
             <li>Matchmaking based on player rating</li>
             <li>Balanced maps and settings</li>
@@ -62,8 +62,6 @@ export default function LeaguePage() {
 
         <div className="@flex">
           <LeaderBoard title="Standard" players={demoData()} />
-          <LeaderBoard title="Fog of War" players={demoData()} />
-          <LeaderBoard title="High Funds" players={demoData()} />
         </div>
       </div>
     </div>
