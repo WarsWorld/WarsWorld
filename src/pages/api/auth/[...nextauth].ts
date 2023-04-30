001110df8d67c6159c03444489b20a0c2a5bb221
MatthewKuKanich <mkukanichgmu@gmail.com>
2023-04-30 02:55:56 -0400

    Update NextAuth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,65 @@
 import NextAuth from "next-auth";
-import { AppProviders } from "next-auth/providers";
 import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
 
-const providers: AppProviders = [];
-const clientId = process.env.DISCORD_CLIENT_ID;
-const clientSecret = process.env.DISCORD_CLIENT_SECRET;
+const providers = [];
 
-if (clientId !== undefined && clientSecret !== undefined) {
+if (process.env.DISCORD_CLIENT_ID && process.env.DISCORD_CLIENT_SECRET) {
   providers.push(
     DiscordProvider({
-      clientId,
-      clientSecret,
-    }),
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    })
   );
 }
 
-export default NextAuth({ providers });
+if (process.env.EMAIL_SERVER && process.env.EMAIL_FROM) {
+  providers.push(
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    })
+  );
+}
+
+if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
+  providers.push(
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    })
+  );
+}
+
+if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
+  providers.push(
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  );
+}
+
+export default NextAuth({
+  providers,
+
+  // The Email provider requires a database, we can comment this provider out until that is setup
+  // database: process.env.DATABASE_URL
+  
+  secret: process.env.SECRET,
+
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout"
+  },
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+  },
+
+  debug: false,
+});
