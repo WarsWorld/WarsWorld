757750448c9ed2e1bd322f468203b7c946e35caf
Javi <96269542+FemboyDeveloper@users.noreply.github.com>
2023-05-05 08:26:22 -0400

    Add comments to create-emitter.ts

--- src/server/emitter/create-emitter.ts
+++ src/server/emitter/create-emitter.ts
@@ -1,3 +1,5 @@
+//read up on JavaScript event emitters. the code here is a typesafe implementation of the same principal. you can subscribe to events with a callback function that will be called when those events occur.
+
 export const createEmitter = <D extends { matchId: string }>() => {
   type Listener = (dispatched: D) => void;
   const listenerMap = new Map<string, Listener[]>();
