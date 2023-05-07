cc52eb92d3c5835444d5ea59feab544c7e932a46
Javi <96269542+FemboyDeveloper@users.noreply.github.com>
2023-05-06 20:02:21 -0400

    Add all countries/armies to armySchema

--- src/server/schemas/army.ts
+++ src/server/schemas/army.ts
@@ -1,11 +1,7 @@
 import { z } from "zod";
 
 export const armySchema = z.enum([
-  "orange-star",
-  "blue-moon",
-  "yellow-comet",
-  "green-earth",
-  "black-hole",
+ "orange-star", "blue-moon", "green-earth", "yellow-comet", "black-hole", "red-fire", "grey-sky", "brown-desert", "amber-blaze", "jade-sun", "cobalt-ice", "pink-cosmos", "teal-galaxy", "purple-lighting", "acid-rain", "white-nova",
 ]);
 
 export const armyWithNeutralSchema = armySchema.or(z.literal("neutral"));
