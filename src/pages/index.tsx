53a02adae9c503588d3820a30546e94cf09e16c3
Ka Hung Lee <lee.kahung414@gmail.com>
2023-06-03 04:19:43 -0700

    Updated Pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,5 @@
 import { Home } from "../frontend/components/Home";
 
 export default function IndexPage() {
-  // const updateDocumentHeightCSSProperty = () => {
-  //   if (typeof window !== 'undefined') {
-  //     const doc = window.document.documentElement;
-  //     doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
-  //   }
-  // };
-
-  // if (typeof window !== 'undefined') {
-  //   window.addEventListener('resize', updateDocumentHeightCSSProperty);
-  //   updateDocumentHeightCSSProperty();
-  // }
-
   return <Home />;
 }
--- src/pages/match/[matchId].tsx
+++ src/pages/match/[matchId].tsx
@@ -1,17 +1,10 @@
-import Head from "next/head";
 import { PixiMatch } from "frontend/components/match/main";
 import { Layout } from "frontend/components/layout";
 
 export default function Match() {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-
-      <Layout>
-        <PixiMatch />
-      </Layout>
-    </>
+    <Layout>
+      <PixiMatch />
+    </Layout>
   );
 }
