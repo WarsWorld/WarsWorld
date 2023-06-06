661996184a5428af3c4f67ec266900f86a377967
Ka Hung Lee <lee.kahung414@gmail.com>
2023-06-06 15:22:26 -0700

    Included responsive fixes are very large screens

--- src/frontend/components/news/FeaturedNews.tsx
+++ src/frontend/components/news/FeaturedNews.tsx
@@ -4,7 +4,7 @@ export function FeaturedNews() {
   return (
     <div className="@flex @justify-center @items-center @w-full">
       <div className="@flex @flex-col smallscreen:@flex-row @cursor-pointer @duration-300 tablet:hover:@shadow-[0_0_10px_10px_rgba(183,234,184,0.5)] tablet:hover:@z-10">
-        <div className="@relatve @h-[300px] @w-full smallscreen:@w-[400px] laptop:@w-[650px] @bg-cover @bg-[url(/img/layout/newsPage/featuredImg.png)]">
+        <div className="@relatve @h-[300px] @w-full smallscreen:@w-[400px] laptop:@w-[60vw] @bg-cover @bg-[url(/img/layout/newsPage/featuredImg.png)]">
           <div className="@h-full @w-full @overflow-hidden @backdrop-brightness-50">
             <Image
               className="@scale-x-[-1]"
@@ -15,7 +15,7 @@ export function FeaturedNews() {
             />
           </div>
         </div>
-        <div className="@flex @flex-col @min-w-[300px] @p-4 @gap-3 @justify-center @bg-bg-secondary smallscreen:@max-w-[300px]">
+        <div className="@flex @flex-col @min-w-[300px] @p-4 @gap-3 @justify-center @bg-bg-secondary smallscreen:@max-w-[300px] laptop:@max-w-[20vw]">
           <div>
             <h3>Welcome back Flak</h3>
             <h1 className="@text-[1.5rem]">Patch 1.10 is out!</h1>
