54bbced2207b6563c9cf0d285a7c4c3a13ef620c
FemboyDeveloper <j1a1b1m1@Gmail.com>
2023-06-19 13:35:23 -0400

    Fix ESLint problems
    Basically added some ignore tags, tried fixing the type-safety issues
    but it seems like a rabbithole im not willing to invest more time into.
    Maybe Function or someone else with more experience can help.

--- src/pages/articles/[id].tsx
+++ src/pages/articles/[id].tsx
@@ -3,7 +3,7 @@ import {
   getArticleIds,
 } from "../../frontend/utils/articleScript";
 import Banner from "../../frontend/components/layout/Banner";
-import styles from "../../frontend/styles/pages/articles.module.scss"
+import styles from "../../frontend/styles/pages/articles.module.scss";
 
 export async function getStaticPaths() {
   const paths = getArticleIds();
@@ -12,8 +12,11 @@ export async function getStaticPaths() {
     fallback: false,
   };
 }
-
+// eslint-disable-next-line
 // @ts-ignore
+//one of you type safety gurus
+//can find out how to solve the getStaticProps
+//params type issue, I could not, so, good luck!
 export async function getStaticProps({ params }) {
   const postData = await getArticleData(params.id);
   return {
@@ -22,20 +25,19 @@ export async function getStaticProps({ params }) {
     },
   };
 }
-
+// eslint-disable-next-line
 // @ts-ignore
+//same issue here, dont know how to
+//appease the TS gods
 export default function Articles({ postData }) {
-  function logStuff(stuff: any) {
-    console.log(stuff);
-  }
-  // eslint-disable-next-line max-len
-  //Lets make sure we have our parameters before loading the page and causing errors
+  //Lets make sure we have our parameters/data
+  // before loading so we dont cause any errors
   if (typeof postData === "undefined") return <h1>Loading...</h1>;
   else {
     //Get headers for index table
     let theHTML: any = postData.contentHtml;
     const headers = [...theHTML.matchAll(/<h1+>(.*?)<\/h1*>/gm)];
-    //Put IDs on headers so /article#header links to the header
+    //Put IDs on headers so /articleName#header links to the header
     for (let i = 0; i < headers.length; i++) {
       theHTML = theHTML.replace(
         /<h1>/,
@@ -43,7 +45,6 @@ export default function Articles({ postData }) {
       );
     }
 
-    // (?s)(?<=<h1>)(.+?)(?=</h1>)
     return (
       <>
         <Banner
@@ -62,33 +63,34 @@ export default function Articles({ postData }) {
           backgroundURL={postData.image}
         />
 
-
-
-        <div className={"@grid @grid-cols-12 smallscreen:@p-10 smallscreen:@gap-10 @gap-2 @relative @leading-10 "}>
-
-
+        <div
+          className={
+            "@grid @grid-cols-12 smallscreen:@p-10 smallscreen:@gap-10 @gap-2 @relative @leading-10 "
+          }
+        >
           <div
-            className={`@col-span-12 smallscreen:@col-span-10 @bg-bg-tertiary smallscreen:@p-10 @p-2 @rounded-2xl ${styles.articleGrid} ` }
+            className={`@col-span-12 smallscreen:@col-span-10 @bg-bg-tertiary smallscreen:@p-10 @p-2 @rounded-2xl ${styles.articleGrid} `}
             dangerouslySetInnerHTML={{ __html: theHTML }}
           />
 
-
-            <div className={" smallscreen:@col-span-2 @col-span-12 @bg-bg-tertiary  @p-3 @sticky @top-[10vw] @h-max @rounded-2xl "}>
-              <p>INDEX</p>
-              {headers.map((item) => {
-                return (
-
-                  <a
-                    href={`#${item[1].replace(/\s/g, "-")}`}
-                    className={"@text-white @block @py-4"}
-                  >
-                    {item[1]}
-                  </a>
-                );
-              })}
-            </div>
-
-
+          <div
+            className={
+              " smallscreen:@col-span-2 @col-span-12 @bg-bg-tertiary  @p-3 @sticky @top-[10vw] @h-max @rounded-2xl "
+            }
+          >
+            <p>INDEX</p>
+            {headers.map((item) => {
+              return (
+                <a
+                  key={item[1]}
+                  href={`#${item[1].replace(/\s/g, "-")}`}
+                  className={"@text-white @block @py-4"}
+                >
+                  {item[1]}
+                </a>
+              );
+            })}
+          </div>
         </div>
       </>
     );
