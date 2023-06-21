f8e5902d0927ce71e76210af11307c0529c004bf
FemboyDeveloper <j1a1b1m1@Gmail.com>
2023-06-21 16:09:41 -0400

    Spritesheet rendering correctly!
    Right now, the pixi.js match is able to render buildings from the
    spritesheet correctly! It still needs some help here and there but its a
    start.

--- src/pages/match/[matchId].tsx
+++ src/pages/match/[matchId].tsx
@@ -1,15 +1,145 @@
-import { PixiMatch } from "frontend/components/match/main";
-import { Layout } from "frontend/components/layout";
-import Head from "next/head";
+import { usePlayers } from "frontend/context/players";
+import { Tile } from "server/schemas/tile";
+import { useRouter } from "next/router";
+import {
+  Application,
+  Assets,
+  BaseTexture,
+  SCALE_MODES,
+  Sprite,
+  Texture,
+  Container,
+  Spritesheet,
+  AnimatedSprite,
+} from "pixijs";
+import { useEffect, useRef, useState } from "react";
+import { PlayerInMatch } from "shared/types/server-match-state";
 
-export default function Match() {
-  return (
-    <>
-      <Head>
-        <title>Game Page | Wars World</title>
-      </Head>
+import { trpc } from "frontend/utils/trpc-client";
 
-      <PixiMatch />
-    </>
-  );
+import getJSON from "../api/spriteSheet/getJSON";
+import { GetStaticPaths } from "next";
+
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+  return {
+    paths: [], //indicates that no page needs be created at build time
+    fallback: "blocking", //indicates the type of fallback
+  };
+};
+
+export async function getStaticProps() {
+  const parsedD = await getJSON("orangeStarBuildings");
+  const parsedData = JSON.parse(parsedD);
+  return {
+    props: {
+      parsedData,
+    },
+  };
 }
+
+BaseTexture.defaultOptions.scaleMode = SCALE_MODES.NEAREST;
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+const Match = ({ parsedData }) => {
+  const { currentPlayer } = usePlayers();
+  const [players, setPlayers] = useState<PlayerInMatch[] | null | undefined>(
+    null
+  );
+  const [mapData, setMapData] = useState<Tile[][] | null | undefined>(null);
+  const pixiCanvasRef = useRef<HTMLCanvasElement>(null);
+
+  const { query } = useRouter();
+  const matchId = query.matchId as string;
+
+  // make trpc call to get data and set it as players and mapData
+  trpc.match.full.useQuery(
+    { matchId, playerId: currentPlayer?.id ?? "" },
+    {
+      enabled: currentPlayer !== undefined,
+      onSuccess(data) {
+        if (data === null) {
+          throw new Error(`Match ${matchId} not found!`);
+        }
+
+        if (!players) {
+          setPlayers(data.players);
+        }
+
+        if (!mapData) {
+          setMapData(data.map.tiles);
+        }
+      },
+    }
+  );
+
+  //Important useEffect to make sure Pixi
+  // only gets updated when pixiCanvasRef or mapData changes
+  // we dont want it to be refreshed in react everytime something changes.
+  useEffect(() => {
+    //logging our mapData just in case you need to see it in the console.
+    console.log(mapData);
+
+    const app = new Application({
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      view: pixiCanvasRef.current,
+      resolution: window.devicePixelRatio || 1,
+      backgroundColor: "#C0E030",
+      width: 500,
+      height: 400,
+    });
+    app.stage.position.set(0, 0);
+
+    //let render our specific cursor
+    const awCursor =
+      'url("http://localhost:3000/img/spriteSheet/cursor.gif"),auto';
+    app.renderer.events.cursorStyles.default = awCursor;
+    const mapContainer = new Container();
+    mapContainer.x = 25;
+    mapContainer.y = 25;
+
+    app.stage.addChild(mapContainer);
+
+    const texture = BaseTexture.from(parsedData.meta.image);
+    const spritesheet = new Spritesheet(texture, parsedData);
+    spritesheet.parse();
+    const buildings = [
+      "Airport",
+      "Base",
+      "City",
+      "ComTower",
+      "HQ",
+      "Lab",
+      "Port",
+    ];
+    buildings.forEach((building, index) => {
+      const tile = new AnimatedSprite(spritesheet.animations[building]);
+      tile.x = index * 32;
+      tile.y = 16;
+      tile.animationSpeed = 0.02;
+      tile.play();
+      mapContainer.addChild(tile);
+    });
+
+    return () => {
+      app.stop();
+    };
+  }, [pixiCanvasRef, mapData]);
+
+  return (
+    <div className={"@m-10"}>
+      <h1>Basic pixi.js dev environment </h1>
+      <h2>Basic rendering</h2>
+      <canvas
+        style={{
+          imageRendering: "pixelated",
+        }}
+        ref={pixiCanvasRef}
+        width={800}
+        height={600}
+      ></canvas>
+    </div>
+  );
+};
+export default Match;
