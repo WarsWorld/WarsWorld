785b3ea5af8a5058e2869e3feb0ab9df682a5b4e
Piero <15440183+pierosavi@users.noreply.github.com>
2023-06-29 20:05:20 +0200

    Improve devcontainer scripts, comment code

--- .devcontainer/prepareDevcontainer.sh
+++ .devcontainer/prepareDevcontainer.sh
@@ -1,14 +1,8 @@
-# Next two lines need because of: https://github.com/devcontainers/features/tree/main/src/node#using-nvm-from-postcreatecommand-or-another-lifecycle-command
+# https://github.com/devcontainers/features/tree/main/src/node#using-nvm-from-postcreatecommand-or-another-lifecycle-command
 . ${NVM_DIR}/nvm.sh
 nvm install 18.16.1
-# Install debian dependencies
-# They are all needed for pixijs -> canvas -> headless-gl dependencies
-# https://github.com/Automattic/node-canvas/issues/1065#issuecomment-654706161
-apt update && apt -y upgrade
-apt install -y pkg-config libpixman-1-dev libcairo2-dev libpango1.0-dev libgif-dev python-is-python3
 # Copy env configuration
 cp .devcontainer/.env.devcontainer .env
 # Setup env
-npm install
+npm ci
 npm run initialise-devcontainer
-npm run dev
