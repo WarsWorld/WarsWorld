075be365d67557fd574fbe65b7743e5cfdca9eae
Piero <15440183+pierosavi@users.noreply.github.com>
2023-06-29 20:46:42 +0200

    Revert dependencies install

--- .devcontainer/prepareDevcontainer.sh
+++ .devcontainer/prepareDevcontainer.sh
@@ -1,8 +1,13 @@
 # https://github.com/devcontainers/features/tree/main/src/node#using-nvm-from-postcreatecommand-or-another-lifecycle-command
 . ${NVM_DIR}/nvm.sh
 nvm install 18.16.1
+# Install debian dependencies
+# They are all needed for pixijs -> canvas -> headless-gl dependencies
+# https://github.com/Automattic/node-canvas/issues/1065#issuecomment-654706161
+apt update && apt -y upgrade
+apt install -y pkg-config libpixman-1-dev libcairo2-dev libpango1.0-dev libgif-dev python-is-python3
 # Copy env configuration
 cp .devcontainer/.env.devcontainer .env
 # Setup env
-npm ci
+npm install
 npm run initialise-devcontainer
