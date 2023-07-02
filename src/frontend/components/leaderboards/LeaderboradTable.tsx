fc66b8446cdd77cf6f5d2c5255f74e26d9b85a7e
Enrique Chávez <56766746+CliveGlitch@users.noreply.github.com>
2023-07-01 19:14:45 -0600

    Leaderboards (#169)
    * Set Up Leaderboards
    
    Page to check the leaderboards and some dummy data.
    
    * Data Table
    
    Leaderboard page added, it shows a very early progress of the data table.
    Installed TanStack Table to make the datatable for leaderboards.
    Documentation: https://tanstack.com/table/v8/docs/guide/introduction
    
    * Leaderboard Table Component created
    
    Component that has the leaderboard data table.
    
    * Responsive DataTable
    
    By installing the react-hook package and doing some tricks the data table will drop some columns or add them depending if the screen width exceeds certain threshold, making the table smaller and more easy to see on mobile devices.
    
    * Basic Filters
    
    Basic Filters
    
    * New Select component for Filters
    
    Select component for filters, not yet connected to the data table but fully functional by their own.
    
    * New Select component for Filters
    
    Select component for filters, not yet connected to the data table but fully functional by their own.
    
    * 100 page pagination
    
    Leaderboards page datatable has a page for ever 100 rows.
    
    * Refactors
    
    Refactors and random data generation.
    
    * Countries and CO's data
    
    Random generation of preferred country and co for each player for the leaderboards table, the table now shows a country logo besides the players names.
    
    * Player name links to profile page
    
    Player's name can now be a link to get to the players profile
    
    * Component Refactors
    
    Made it so the DataTable component, that manages how the table looks. can be reused for any other data table.
    Some name changes for some components.
    
    * Footer for the datatable
    
    Added the possibility to add a footer to the dataTable.
    
    * Leaderboard best player section
    
    Early version of the top player section completed.
    
    * Quick changes to make Best Players Section Responsive
    
    * Responsive Best Players Section and Refactors
    
    Best Player Section is now more responsive.
    Refactors to the whole leaderboards page to be divided into more components, like one for pagination, filters and best players section.
    
    * Responsive touch ups
    
    Changed some things to make the table, filters, players section and buttons to be more responsive to smaller devices.
    
    * Unnecessary image
    
    ---------
    
    Co-authored-by: Javi <96269542+FemmDev@users.noreply.github.com>

---
+++ src/frontend/components/leaderboards/LeaderboradTable.tsx
@@ -0,0 +1,80 @@
+// Read TanStack Table documentation to understand how the
+// leaderboards table is generated:
+// https://tanstack.com/table/v8/docs/guide/introduction
+
+import { useState, useEffect } from "react";
+import {
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+  Table,
+} from "@tanstack/react-table";
+import getLeaderboardData, { PlayerLeaderboard } from "./LeaderboardData";
+import { columns } from "./LeaderboardColumns";
+import { useWindowWidth } from "@react-hook/window-size";
+import DataTable from "../layout/DataTable";
+import TablePagination from "../layout/TablePagination";
+import { SelectOption } from "../layout/Select";
+
+function hideColumns(table: Table<PlayerLeaderboard>, screenWidth: number) {
+  const columnsToHide = ["Games", "Win Rate", "Streak"];
+  if (screenWidth <= 768) {
+    table
+      .getAllLeafColumns()
+      .filter((column) => columnsToHide.includes(column.id))
+      .map((column) => {
+        column.toggleVisibility(false);
+      });
+  } else {
+    table
+      .getAllLeafColumns()
+      .filter((column) => columnsToHide.includes(column.id))
+      .map((column) => {
+        column.toggleVisibility(true);
+      });
+  }
+}
+
+interface Props {
+  setBestPlayers: React.Dispatch<React.SetStateAction<PlayerLeaderboard[]>>;
+  gamemode: SelectOption | undefined;
+  timeMode: SelectOption | undefined;
+}
+
+export default function LeaderboardTable({ setBestPlayers }: Props) {
+  const screenWidth = useWindowWidth();
+  const [columnVisibility, setColumnVisibility] = useState({});
+  const [data, setData] = useState([] as PlayerLeaderboard[]);
+
+  // Use this to rerender the table, probably better used for filter changes
+  //const rerender = useReducer(() => ({}), {})[1];
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      columnVisibility,
+    },
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  useEffect(() => {
+    const requestedData = getLeaderboardData(550);
+    setBestPlayers(requestedData.filter((player) => player.rank <= 6));
+    setData(requestedData);
+    // Set the amount of rows the table has for each page
+    table.setPageSize(100);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  // makes the table more responsive by removing and adding columns
+  useEffect(() => hideColumns(table, screenWidth), [screenWidth, table]);
+
+  return (
+    <div className="@flex @flex-col @w-full @items-center @justify-center @mb-12 smallscreen:@mb-20 @min-w-[90vw] tablet:@min-w-[80vw]">
+      <DataTable table={table} />
+      <TablePagination table={table} />
+    </div>
+  );
+}
---
+++ src/pages/leaderboard.tsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import ThreeLinesText from "frontend/components/layout/ThreeLinesText";
+import LeaderboardTable from "../frontend/components/leaderboards/LeaderboradTable";
+import { SelectOption } from "frontend/components/layout/Select";
+import { useState } from "react";
+import BestPlayersSection from "frontend/components/leaderboards/BestPlayersSection";
+import { PlayerLeaderboard } from "frontend/components/leaderboards/LeaderboardData";
+import LeaderboardFilters from "frontend/components/leaderboards/LeaderboardFilters";
+
+export default function IndexPage() {
+  const [bestPlayers, setBestPlayers] = useState([] as PlayerLeaderboard[]);
+  const [gamemode, setGamemode] = useState<SelectOption | undefined>({
+    label: "All",
+    value: 0,
+  });
+  const [timeMode, setTimeMode] = useState<SelectOption | undefined>({
+    label: "All",
+    value: 0,
+  });
+
+  return (
+    <div className="@flex @flex-col @w-[100vw] @items-center @justify-center">
+      <Head>
+        <title>Leaderboards</title>
+      </Head>
+
+      <ThreeLinesText
+        subtitle="Where the best meet"
+        title="Leaderboards"
+        text=""
+      />
+
+      <div className="@mb-4">
+        <BestPlayersSection bestPlayers={bestPlayers} />
+      </div>
+
+      <div className="@flex @flex-col @max-w-full @px-4">
+        <LeaderboardFilters
+          gamemode={gamemode}
+          setGamemode={setGamemode}
+          timeMode={timeMode}
+          setTimeMode={setTimeMode}
+        />
+        <LeaderboardTable
+          setBestPlayers={setBestPlayers}
+          gamemode={gamemode}
+          timeMode={timeMode}
+        />
+      </div>
+    </div>
+  );
+}
