feb050b305e3dd5baa26bea8bf000b2af712081a
Javi <96269542+FemmDev@users.noreply.github.com>
2023-07-04 14:24:01 -0400

    Render game menu correctly (#172)
    * Spritesheet rendering correctly!
    
    Right now, the pixi.js match is able to render buildings from the
    spritesheet correctly! It still needs some help here and there but its a
    start.
    
    * Fix ESLINT, use SSR over SSG
    
    * Optimize spritesheets, render about 80% of Map
    
    Spritesheets
    now dont include a -0 at the end, were optimized to be all lowercase
    (thanks chatgpt with your python script).
    
    Now we are rendering almost everything, just need to add a sea and shoal
    tile, fix the names of some tiles and it will be all perfect!
    
    * Fix typos in map-importer, tweak sprite sheets and finish rendering CF
    
    Caustic finale has been rendered completely and correctly now! So we now
    have a basic pixi.js environment completely setup and running! Next
    thing will be rendering menus and being able to build units.
    
    * Fix getJSON bug, add other maps
    
    Now we can play any map, no matter the size of tile as long as it is a
    blue moon vs orange star match (havent added other countries).
    
    This small bugfix should make the whole ordeals of map rendering much
    better since now we have a conistent performance.
    
    * Fix ESLint
    
    * Start of game-menu
    
    Starting the first steps of the game menu, currently it renders a very
    basic design and its on its very start. Needs alot of polish.
    
    * Polishing menu animaiton
    
    * Minor Update
    
    * Minor Commit
    
    * Still testing pixi stuff, radical changes incoming
    
    Changed how we display our stage and app. Received alot of input and advice from pixi.js devs.
    
    * Add alot of comments, complete menu rendering
    
    Now the whole match and showMenu pages both have alot of doucmentation
    and work properly.
    
    * Include package JSON, fix eslint
    
    * Fix pixi.js import typo
    
    a file had pixijs rather than pixi.js.

--- src/pages/match/[matchId].tsx
+++ src/pages/match/[matchId].tsx
@@ -1,5 +1,6 @@
 //TODO: Fix TS type issues, TS is getting angry at very complex types
-// Im not going to bother going on rabbit holes to please the TS gods.
+// Im not going to bother going on rabbit holes to please the TS gods
+// and their confusing requests
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
@@ -7,20 +8,20 @@ import { usePlayers } from "frontend/context/players";
 import { Tile } from "server/schemas/tile";
 import { useRouter } from "next/router";
 import {
+  AnimatedSprite,
   Application,
   BaseTexture,
+  Container,
   SCALE_MODES,
   Sprite,
-  Container,
   Spritesheet,
-  AnimatedSprite,
-} from "pixijs";
+} from "pixi.js";
 import { useEffect, useRef, useState } from "react";
-import { Layer } from "@pixi/layers";
 import { PlayerInMatch } from "shared/types/server-match-state";
 
 import { trpc } from "frontend/utils/trpc-client";
 import getJSON from "../../spriteSheet/getJSON";
+import showMenu from "../../spriteSheet/showMenu";
 
 BaseTexture.defaultOptions.scaleMode = SCALE_MODES.NEAREST;
 
@@ -62,31 +63,35 @@ const Match = ({ spriteData }) => {
   useEffect(() => {
     const app = new Application({
       view: pixiCanvasRef.current,
-      resolution: 2,
-      backgroundColor: "#3a4817",
+      autoDensity: true,
+      resolution: window.devicePixelRatio,
+      backgroundColor: "#061838",
       //TODO: The width needs to be = mapData[0].length * 16 + 16, but it seems it errors out if mapData isnt loaded well.
       // However, mapData?.length seems to work well for the height.
-      width: 16 * 30 + 16,
-      height: 16 * mapData?.length + 16,
+      width: 1400,
+      height: 2000,
+      resizeTo: undefined,
     });
-    //8 is half of 16, currently our border half a tile. Needed so mountains and cities display fully at the top.
-    app.stage.position.set(0, 8);
+
+    //TODO: Button with + and - to change the scale of our stage, also needs
+    // to have app.resize() working so we can resize the size of our app.
+    app.stage.scale.set(2.6, 2.6);
+    app.stage.position.set(0, 16);
 
     //let render our specific cursor
+    //TODO: Cursor stops working on half screen?
     app.renderer.events.cursorStyles.default = {
       animation: "gameCursor 1200ms infinite",
     };
-
+    //the container that holds everything
     const mapContainer = new Container();
-    mapContainer.x = 0;
-    mapContainer.y = 0;
-    //allows for us to use zIndex
+
+    //allows for us to use zIndex on the children of mapContainer
     mapContainer.sortableChildren = true;
     app.stage.addChild(mapContainer);
 
+    //Lets create our spritesheets/map the image with the json!
     const spriteSheets: Spritesheet[] = [];
-    console.log(spriteData.countries);
-
     spriteData.countries.forEach((country: string) => {
       const texture = BaseTexture.from(spriteData[country].meta.image);
       const sheet = new Spritesheet(texture, spriteData[country]);
@@ -94,7 +99,8 @@ const Match = ({ spriteData }) => {
       spriteSheets.push(sheet);
     });
 
-    if (mapData != undefined || mapData != null) {
+    //Lets render our map!
+    if (mapData != undefined) {
       let tile;
       mapData.forEach((col, colIndex) => {
         mapData[colIndex].forEach((row, rowIndex) => {
@@ -109,9 +115,36 @@ const Match = ({ spriteData }) => {
               //NOT NEUTRAL
             } else {
               tile = new AnimatedSprite(spriteSheets[slot].animations[type]);
+              //if our building is able to produce units, it has a menu!
+              if (type !== "hq" && type !== "lab" && type !== "city") {
+                tile.eventMode = "static";
+                //Lets make menu appear
+                tile.on("pointerdown", async () => {
+                  console.log("touched an action tile!");
+                  const menu = await showMenu(
+                    spriteSheets[slot],
+                    type,
+                    slot,
+                    rowIndex,
+                    colIndex
+                  );
+                  //lets make menu dissapear on hover out
+                  //TODO: Make menu dissapear if we click somewhere else
+                  menu.on("pointerleave", () => {
+                    console.log("menu pointerout");
+                    const length = mapContainer.children.length;
+                    mapContainer.removeChild(mapContainer.children[length - 1]);
+                  });
+                  mapContainer.addChild(menu);
+                });
+              }
+
               //TODO: Seems like properties/buildings have different animation speeds...
+              // gotta figure out how to make sure all buildings are animated properly
+              // or at least AWBW seems to have different speeds/frames than Daemon's replayer
               tile.animationSpeed = 0.03;
               tile.play();
+              console.log(window.devicePixelRatio);
             }
 
             //NOT A PROPERTY
@@ -124,7 +157,6 @@ const Match = ({ spriteData }) => {
           }
           //makes our sprites render at the bottom, not from the top.
           tile.anchor.set(0.5, 1);
-          //tile.zIndex = (mapData.length - colIndex) * 10;
           tile.x = (rowIndex + 1) * 16;
           tile.y = (colIndex + 1) * 16;
           mapContainer.addChild(tile);
@@ -137,6 +169,7 @@ const Match = ({ spriteData }) => {
     };
   }, [pixiCanvasRef, mapData, spriteData]);
 
+  //Actual return statement for react function
   if (!spriteData) return <h1>Loading...</h1>;
   else {
     return (
@@ -153,6 +186,7 @@ const Match = ({ spriteData }) => {
   }
 };
 export default Match;
+
 
 export async function getServerSideProps() {
   //TODO: Should we call all the spritesheets or just the ones the players will need?
