5d1777a9f9c14d49509d84134d839a11bcaa1536
FemboyDeveloper <j1a1b1m1@Gmail.com>
2023-07-09 16:46:21 -0400

    Deleted unused component, fix other minor issues
    Deleted a component that would create a plus and minus button on pixi,
    this is now handled outside of pixi.
    
    Als

--- src/pages/match/[matchId].tsx
+++ src/pages/match/[matchId].tsx
@@ -24,7 +24,6 @@ import { trpc } from "frontend/utils/trpc-client";
 import getJSON from "../../gameFunction/getJSON";
 import showMenu from "../../gameFunction/showMenu";
 import spriteConstructor from "../../gameFunction/spriteConstructor";
-import plusMinusButtons from "../../gameFunction/components/plusMinusButtons";
 
 BaseTexture.defaultOptions.scaleMode = SCALE_MODES.NEAREST;
 
@@ -63,15 +62,11 @@ const Match = ({ spriteData }) => {
   //Important useEffect to make sure Pixi
   // only gets updated when pixiCanvasRef or mapData changes
   // we dont want it to be refreshed in react everytime something changes.
-
-  //TODO: RECalculate width, height and scale based on third party variable
   const [scale, setScale] = useState<number>(1.8);
 
   useEffect(() => {
     if (mapData) {
-
-
-      const mapScale = scale * 16
+      const mapScale = scale * 16;
       const mapMargin = scale * 32;
       const app = new Application({
         view: pixiCanvasRef.current,
@@ -84,7 +79,6 @@ const Match = ({ spriteData }) => {
       app.stage.position.set(0, 0);
       app.stage.sortableChildren = true;
 
-
       //let render our specific cursor
       //TODO: Cursor stops working on half screen on google chrome (works on firefox).
       app.renderer.events.cursorStyles.default = {
@@ -93,7 +87,6 @@ const Match = ({ spriteData }) => {
 
       //the container that holds the map
       const mapContainer = new Container();
-
       mapContainer.x = 16;
       mapContainer.y = 16;
 
@@ -101,9 +94,6 @@ const Match = ({ spriteData }) => {
       mapContainer.sortableChildren = true;
       app.stage.scale.set(scale, scale);
       app.stage.addChild(mapContainer);
-
-      //lets add our buttons
-      //app.stage.addChild(plusMinusButtons(mapContainer));
 
       //Lets create our spritesheets/map the image with the json!
       const spriteSheets: Spritesheet[] = [];
@@ -115,89 +105,87 @@ const Match = ({ spriteData }) => {
       });
 
       //Lets render our map!
-      if (mapData != undefined) {
-        let tile;
-        mapData.forEach((col, colIndex) => {
-          mapData[colIndex].forEach((row, rowIndex) => {
-            const type = row.type;
-            //ITS A PROPERTY
-            if (row.hasOwnProperty("playerSlot")) {
-              const slot: number = row.playerSlot;
+      let tile;
+      mapData.forEach((col, colIndex) => {
+        mapData[colIndex].forEach((row, rowIndex) => {
+          const type = row.type;
+          //ITS A PROPERTY
+          if (row.hasOwnProperty("playerSlot")) {
+            const slot: number = row.playerSlot;
 
-              //NEUTRAL
-              if (row.playerSlot === -1) {
-                tile = new Sprite(spriteSheets[2].textures[type + "-0.png"]);
-                //NOT NEUTRAL
-              } else {
-                tile = new AnimatedSprite(spriteSheets[slot].animations[type]);
-                //if our building is able to produce units, it has a menu!
-                if (type !== "hq" && type !== "lab" && type !== "city") {
-                  tile.eventMode = "static";
-                  //Lets make menu appear
-                  tile.on("pointerdown", async () => {
-                    const menu = await showMenu(
-                      spriteSheets[slot],
-                      type,
-                      slot,
-                      rowIndex,
-                      colIndex,
-                      mapData.length - 1,
-                      mapData[0].length - 1
-                    );
+            //NEUTRAL
+            if (row.playerSlot === -1) {
+              tile = new Sprite(spriteSheets[2].textures[type + "-0.png"]);
+              //NOT NEUTRAL
+            } else {
+              tile = new AnimatedSprite(spriteSheets[slot].animations[type]);
+              //if our building is able to produce units, it has a menu!
+              if (type !== "hq" && type !== "lab" && type !== "city") {
+                tile.eventMode = "static";
+                //Lets make menu appear
+                tile.on("pointerdown", async () => {
+                  const menu = await showMenu(
+                    spriteSheets[slot],
+                    type,
+                    slot,
+                    rowIndex,
+                    colIndex,
+                    mapData.length - 1,
+                    mapData[0].length - 1
+                  );
 
-                    //if there is a menu already out, lets remove it
-                    mapContainer.removeChild(mapContainer.getChildByName("menu"));
+                  //if there is a menu already out, lets remove it
+                  mapContainer.removeChild(mapContainer.getChildByName("menu"));
 
-                    //lets create a transparent screen that covers everything.
-                    // if we click on it, we will delete the menu
-                    // therefore, achieving a quick way to delete menu if we click out of it
-                    const emptyScreen = spriteConstructor(
-                      Texture.WHITE,
-                      0,
-                      0,
-                      app.stage.width,
-                      app.stage.height,
-                      "static",
-                      -1
-                    );
-                    emptyScreen.alpha = 0;
+                  //lets create a transparent screen that covers everything.
+                  // if we click on it, we will delete the menu
+                  // therefore, achieving a quick way to delete menu if we click out of it
+                  const emptyScreen = spriteConstructor(
+                    Texture.WHITE,
+                    0,
+                    0,
+                    app.stage.width,
+                    app.stage.height,
+                    "static",
+                    -1
+                  );
+                  emptyScreen.alpha = 0;
 
-                    emptyScreen.on("pointerdown", (event) => {
-                      mapContainer.removeChild(menu);
-                      mapContainer.removeChild(emptyScreen);
-                    });
-                    mapContainer.addChild(menu);
-                    mapContainer.addChild(emptyScreen);
+                  emptyScreen.on("pointerdown", (event) => {
+                    mapContainer.removeChild(menu);
+                    mapContainer.removeChild(emptyScreen);
                   });
-                }
-
-                //TODO: Seems like properties/buildings have different animation speeds...
-                // gotta figure out how to make sure all buildings are animated properly
-                // or at least AWBW seems to have different speeds/frames than Daemon's replayer
-                tile.animationSpeed = 0.04;
-                tile.play();
+                  mapContainer.addChild(menu);
+                  mapContainer.addChild(emptyScreen);
+                });
               }
 
-              //NOT A PROPERTY
-            } else {
-              if (row.hasOwnProperty("variant"))
-                tile = new Sprite(
-                  spriteSheets[2].textures[row.type + "-" + row.variant + ".png"]
-                );
-              else tile = new Sprite(spriteSheets[2].textures[row.type + ".png"]);
+              //TODO: Seems like properties/buildings have different animation speeds...
+              // gotta figure out how to make sure all buildings are animated properly
+              // or at least AWBW seems to have different speeds/frames than Daemon's replayer
+              tile.animationSpeed = 0.04;
+              tile.play();
             }
-            tile.anchor.set(1, 1);
-            tile.x = (rowIndex + 1) * 16;
-            tile.y = (colIndex + 1) * 16;
-            mapContainer.addChild(tile);
-          });
+
+            //NOT A PROPERTY
+          } else {
+            if (row.hasOwnProperty("variant"))
+              tile = new Sprite(
+                spriteSheets[2].textures[row.type + "-" + row.variant + ".png"]
+              );
+            else tile = new Sprite(spriteSheets[2].textures[row.type + ".png"]);
+          }
+          tile.anchor.set(1, 1);
+          tile.x = (rowIndex + 1) * 16;
+          tile.y = (colIndex + 1) * 16;
+          mapContainer.addChild(tile);
         });
-      }
-      console.log(mapData);
-      return () => {
-        app.stop();
-      };
+      });
     }
+    console.log(mapData);
+    return () => {
+      app.stop();
+    };
   }, [pixiCanvasRef, mapData, spriteData, scale]);
 
   //Actual return statement for react function
@@ -205,36 +193,43 @@ const Match = ({ spriteData }) => {
   else {
     return (
       <div className="@grid @grid-cols-12  @text-center @my-20">
-
         <div className="@col-span-12 @p-2">
-          <button className={"btn @inline"} onClick={()=>{
-            setScale(scale + 0.2);
-          }}>+</button>
-          <h2 className="@inline @align-middle"> {Math.round(scale * 10)/10} </h2>
-          <button className={"btn"} onClick={()=>{
-            setScale(scale - 0.2);
-          }}>-</button>
+          <button
+            className={"btn @inline"}
+            onClick={() => {
+              setScale(scale + 0.2);
+            }}
+          >
+            +
+          </button>
+          <h2 className="@inline @align-middle">
+            {" "}
+            {Math.round(scale * 10) / 10}{" "}
+          </h2>
+          <button
+            className={"btn"}
+            onClick={() => {
+              setScale(scale - 0.2);
+            }}
+          >
+            -
+          </button>
         </div>
 
-<div className="@col-span-12">
-  <canvas className="@inline"
-          style={{
-            imageRendering: "pixelated",
-          }}
-          ref={pixiCanvasRef}
-  ></canvas>
-</div>
-
-
-
-
+        <div className="@col-span-12">
+          <canvas
+            className="@inline"
+            style={{
+              imageRendering: "pixelated",
+            }}
+            ref={pixiCanvasRef}
+          ></canvas>
+        </div>
       </div>
     );
   }
-
 };
 export default Match;
-
 
 export async function getServerSideProps() {
   //TODO: Should we call all the spritesheets or just the ones the players will need?
