2e0515d09ba26c4e2bbd4d3963ca947875124ebb
FunctionDJ <34871211+FunctionDJ@users.noreply.github.com>
2023-11-29 23:57:17 +0100

    add sturm meteor strike TODO for storing position

--- src/shared/match-logic/cos/sturm/sturm-aw2.ts
+++ src/shared/match-logic/cos/sturm/sturm-aw2.ts
@@ -5,7 +5,8 @@ export const sturmAW2: COProperties = {
   displayName: "Sturm",
   gameVersion: "AW2",
   dayToDay: {
-    description: "Units have +20% firepower and +20% defense. All terrain movement cost is reduced to 1 (doesn't apply in snow).",
+    description:
+      "Units have +20% firepower and +20% defense. All terrain movement cost is reduced to 1 (doesn't apply in snow).",
     hooks: {
       movementCost: (_value, match) => {
         if (match.currentWeather !== "snow") {
@@ -17,11 +18,15 @@ export const sturmAW2: COProperties = {
     }
   },
   powers: {
+    // TODO important! meteor strike cannot be deterministic for clients because
+    // clients don't see into fog or hidden sub/stealth, which meteor strike ignores.
+    // therefore the power event must be extended by the impact position.
     superCOPower: {
       name: "Meteor Strike",
-      description: "Units gain an additional +20% firepower and +20% defense. Deals 8 HP of damage to all units at a distance less or equal than 2 from the chosen position, centered on a unit. The meteor prioritises the most unit value in damages (allied units are dealt damage as well, and contribute negatively to the unit value calculation).",
+      description:
+        "Units gain an additional +20% firepower and +20% defense. Deals 8 HP of damage to all units at a distance less or equal than 2 from the chosen position, centered on a unit. The meteor prioritises the most unit value in damages (allied units are dealt damage as well, and contribute negatively to the unit value calculation).",
       stars: 10,
-      instantEffect( {match, player} ) {
+      instantEffect({ match, player }) {
         match.units.damageUntil1HPInRadius({
           radius: 2,
           damageAmount: 8,
