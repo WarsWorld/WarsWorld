932ed12b2d7243cf52bbf9171e57d8ed62da92d3
Pau <paumartibiosca@gmail.com>
2023-12-11 11:50:35 +0100

    Fixed hawke powers, which heal without rounding up

--- src/shared/match-logic/game-constants/cos/hawke/hawke-aw2.ts
+++ src/shared/match-logic/game-constants/cos/hawke/hawke-aw2.ts
@@ -10,13 +10,19 @@ export const hawkeAW2: COProperties = {
     }
   },
   powers: {
+    // IMPORTANT: only type of healing that does not round up!
     COPower: {
       name: "Black Wave",
       stars: 5,
       description:
         "All units heal 1 HP, and all enemy units lose 1 HP (to a minimum of 0.1HP).",
       instantEffect(player) {
-        player.getUnits().forEach((unit) => unit.heal(1));
+        player.getUnits().forEach((unit) => {
+          // need to add this hidden check, but units will never be hidden cause they are hawke's units, not sonja's
+          if (unit.data.stats !== "hidden") {
+            unit.data.stats.hp = Math.min(unit.data.stats.hp + 10, 100);
+          }
+        });
         player.team.getEnemyUnits().forEach(unit => unit.damageUntil1HP(1));
       }
     },
@@ -26,7 +32,12 @@ export const hawkeAW2: COProperties = {
       description:
         "All units heal 2 HP, and all enemy units lose 2 HP (to a minimum of 0.1HP).",
       instantEffect(player) {
-        player.getUnits().forEach((unit) => unit.heal(2));
+        player.getUnits().forEach((unit) => {
+          // need to add this hidden check, but units will never be hidden cause they are hawke's units, not sonja's
+          if (unit.data.stats !== "hidden") {
+            unit.data.stats.hp = Math.min(unit.data.stats.hp + 20, 100);
+          }
+        });
         player.team.getEnemyUnits().forEach(unit => unit.damageUntil1HP(2));
       }
     }
