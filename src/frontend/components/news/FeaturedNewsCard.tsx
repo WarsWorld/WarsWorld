96109942b72acf3380fd1ce97578c3a1e1f04e8f
Enrique Chávez <56766746+CliveGlitch@users.noreply.github.com>
2024-01-06 16:09:54 -0600

    NEW: Responsive featured news.

--- src/frontend/components/news/FeaturedNewsCard.tsx
+++ src/frontend/components/news/FeaturedNewsCard.tsx
@@ -18,21 +18,21 @@ type Props = {
 
 export function FeaturedNewsCard({ cardInfo } : Props) {
   return (
-    <div className="@w-[101%] @h-[70vh] @-translate-x-2 hover:@translate-x-0 @duration-200 @ease-in-out">
+    <div className="@w-full laptop:@w-[101%] @h-[36rem] smallscreen:@h-[65vh] laptop:@-translate-x-2 laptop:hover:@translate-x-0 hover:@-translate-y-1 laptop:hover:@translate-y-0 @duration-200 @ease-in-out">
       <Link href={`${cardInfo?.subdirectory}`} className="@w-full @z-10">
       <OrangeGradientLine />
-      <div className="@flex @justify-between @items-center @w-full @bg-black/50 @h-full hover:@bg-bg-primary/90 @duration-200 @ease-in">
-        <div className="@relative @w-[30%] @h-full @py-8 @ml-16">
-          <h2 className="@text-6xl @font-semibold @my-8">{cardInfo?.title}</h2>
+      <div className="@flex @flex-col-reverse laptop:@flex-row @justify-between @items-center @w-full @bg-black/50 @h-full hover:@bg-bg-primary/90 @duration-200 @ease-in">
+        <div className="@relative @w-full laptop:@w-[40%] monitor:@w-[30%] @h-full @px-4 laptop:@py-8 laptop:@ml-16">
+          <h2 className="@text-4xl monitor:@text-6xl @font-semibold @my-4 monitor:@my-8">{cardInfo?.title}</h2>
           <div className="@text-white">
-            <p className="@text-2xl">{cardInfo?.description}</p>
-            <p className="@absolute @bottom-10 @text-2xl">{cardInfo?.date}</p>
-            <p className="@absolute @bottom-10 @right-12 @text-2xl">{cardInfo?.category}</p>
+            <p className="@text-xl laptop:@text-2xl smalscreen:@my-2">{cardInfo?.description}</p>
+            <p className="@absolute @bottom-4 laptop:@bottom-10 @text-2xl">{cardInfo?.date}</p>
+            <p className="@absolute @bottom-4 laptop:@bottom-10 @right-10 @text-2xl">{cardInfo?.category}</p>
           </div>
         </div>
-        <div className="@w-auto @h-full @p-1 @mx-10 @mr-16">
+        <div className="@w-full laptop:@w-auto laptop:@h-full @p-1 laptop:@mx-10 laptop:@mr-16">
           <Image
-              className="@grid-rows-1 @w-auto @h-full @object-cover @object-top"
+              className="@grid-rows-1 @w-full laptop:@w-auto @h-auto laptop:@h-full @object-cover @object-top"
               src={cardInfo?.image}
               alt={cardInfo?.imageAlt}
               width={0}
