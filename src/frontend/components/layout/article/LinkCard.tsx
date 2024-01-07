5e916c0c4f0f54366914d73a87f6cd51c2266a76
Enrique Chávez <56766746+CliveGlitch@users.noreply.github.com>
2024-01-06 19:29:15 -0600

    CHANGE: Fixed sized link cards.
    Card thumbnails now have to cover the standard ratio of 1280 x 720. That means the images won't be different depending of screens besides pixel count.

--- src/frontend/components/layout/article/LinkCard.tsx
+++ src/frontend/components/layout/article/LinkCard.tsx
@@ -17,22 +17,19 @@ type Props = {
 
 export default function LinkCard({ cardInfo }: Props) {
   return (
-    <div className="@relative @bg-black/50 @object-cover @h-[478px] @w-full @transform @cursor-pointer @border-transparent @border-2 hover:@-translate-y-1 hover:@border-primary tablet:hover:@z-10 hover:@bg-bg-primary/90 @duration-200 @ease-in">
+    <div className="@relative @bg-black/50 @object-cover @h-[380px] laptop:@h-[478px] @w-[320px] laptop:@w-[400px] @transform @cursor-pointer @border-transparent @border-2 hover:@-translate-y-1 hover:@border-primary tablet:hover:@z-10 hover:@bg-bg-primary/90 @duration-200 @ease-in">
       <Link href={`${cardInfo.subdirectory}`} className="@absolute @h-full @w-full @z-10" />
       <div className="@grid @grid-rows-2 @h-full">
-        <Image
-          className="@grid-rows-1 @w-full @h-full @object-cover @object-top"
+        <img
+          className="@grid-rows-1 @w-[320px] @h-[180px] laptop:@w-[400px] laptop:@h-[225px] @object-cover"
           src={cardInfo.image}
           alt={cardInfo.imageAlt}
-          width={0}
-          height={0}
-          sizes="100vw"
         />
-        <div className="@relative @grid-rows-1 @h-full @px-2 laptop:@px-4 @py-4 laptop:@pb-4">
-          <h2 className="@text-[2.5vh] @font-semibold">{cardInfo.title}</h2>
+        <div className="@relative @grid-rows-1 @h-full @px-2 laptop:@px-4 laptop:@pb-4">
+          <h2 className="@text-2xl @font-semibold">{cardInfo.title}</h2>
           <p className="">{cardInfo.description}</p>
-          <p className="@absolute @bottom-4 @right-4">{cardInfo.date}</p>
-          <p className="@absolute @bottom-4 @left-4">{cardInfo.category}</p>
+          <p className="@absolute @bottom-2 laptop:@bottom-4 @right-2 laptop:@right-4">{cardInfo.date}</p>
+          <p className="@absolute @bottom-2 laptop:@bottom-4 @left-2 laptop:@left-4">{cardInfo.category}</p>
           {/* <p className="@h-full">{cardInfo.text}</p> */}
         </div>
       </div>
--- src/frontend/components/layout/article/LinkCardContainer.tsx
+++ src/frontend/components/layout/article/LinkCardContainer.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function LinkCardContainer({ children }: Props) {
   return (
-      <div className="@flex @flex-col @gap-8 @justify-center @items-center @my-4 smallscreen:@grid smallscreen:@grid-flow-row smallscreen:@grid-cols-2 laptop:@grid-cols-3 monitor:@grid-cols-4 large_monitor:@grid-cols-5">
+      <div className="@flex @flex-col @gap-4 desktop:@gap-8 @justify-center @items-center @my-4 smallscreen:@grid smallscreen:@grid-flow-row smallscreen:@grid-cols-2 desktop:@grid-cols-3 monitor:@grid-cols-4 large_monitor:@grid-cols-5">
         {children}
       </div>
   );
