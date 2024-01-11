dc62856956504c3f5a35d33f4b907e66ec7802d0
Enrique <56766746+cliveglitch@users.noreply.github.com>
2024-01-11 08:32:53 -0600

    NEW: Quick 404 error page

--- src/frontend/components/layout.tsx
+++ src/frontend/components/layout.tsx
@@ -11,8 +11,8 @@ export function Layout({ footer, children }: Props) {
   return (
     <>
       <Navbar />
-      <div className="@relative mainContent">
-        <main className="@flex @flex-col @w-full @items-center @justify-center @h-full">
+      <div className="@relative mainContainer">
+        <main className="@flex @flex-col @w-full @items-center @justify-center mainHeight">
           {children}
         </main>
       </div>
