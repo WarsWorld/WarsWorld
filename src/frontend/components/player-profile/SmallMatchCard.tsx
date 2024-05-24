36f96889393f11436e6aa2dbf60f933b35ed4fb3
Enrique Chavez <56766746+cliveglitch@users.noreply.github.com>
2024-05-24 16:55:46 -0600

    WIP: Friendship section!

--- src/frontend/components/player-profile/SmallMatchCard.tsx
+++ src/frontend/components/player-profile/SmallMatchCard.tsx
@@ -25,23 +25,23 @@ export default function SmallMatchCard({ matchResult, player1, player2, matchLin
       href={matchLink}
     >
       <div
-        className={`@flex ${resultColors[matchResult]} @h-full @min-w-20 @font-russoOne @text-4xl @align-middle @justify-center @items-center`}
+        className={`@flex ${resultColors[matchResult]} @h-full @min-w-16 @font-russoOne @text-4xl @align-middle @justify-center @items-center`}
       >
         <strong>{matchResult}</strong>
       </div>
       <div className="@grid @grid-cols-2 @w-full @mx-2">
         <div className="@flex @items-center">
           <img
-            className="[image-rendering:pixelated] @grayscale @h-16"
+            className="[image-rendering:pixelated] @grayscale @h-12"
             src={`/img/CO/pixelated/${player1.coId.name}-small.png`}
             alt={player1.coId.name}
           />
-          <p className="@self-start @px-2">{player1.name}</p>
+          <p className="@self-start @px-2 @text-sm">{player1.name}</p>
         </div>
         <div className="@flex @items-center @justify-end">
-          <p className="@self-end @px-2">{player2.name}</p>
+          <p className="@self-end @px-2 @text-sm">{player2.name}</p>
           <img
-            className="[image-rendering:pixelated] @scale-x-[-1] @h-16"
+            className="[image-rendering:pixelated] @scale-x-[-1] @h-12"
             src={`/img/CO/pixelated/${player2.coId.name}-small.png`}
             alt={player2.coId.name}
           />
--- src/pages/players/[playerName].tsx
+++ src/pages/players/[playerName].tsx
@@ -5,6 +5,7 @@ import MMRDataTable from "frontend/components/player-profile/MMRDataTable";
 import { columns } from "frontend/components/player-profile/MMRTableColumns";
 import PlayerMMRCard from "frontend/components/player-profile/PlayerMMRCard";
 import SmallMatchCard from "frontend/components/player-profile/SmallMatchCard";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -84,133 +85,152 @@ export default function UserProfile() {
   const playerName = query?.playerName;
 
   return (
-    <div className="@w-[90vw] @my-4">
-      <section className="@h-full @bg-black/60 @my-4 @rounded-t-xl @overflow-hidden">
-        <div className="@h-4 @w-full @bg-blue-moon" />
-        <div className="@flex @space-x-12 @px-12 @py-10">
-          <div className="@min-w-48 @h-48 @border-blue-moon @bg-black/50 @border-4 @text-center @overflow-hidden">
-            <img src="\img\CO\smoothFull\Awds-grit.webp" alt="grit" />
-          </div>
-          <div className="@min-h-48 @flex @flex-col">
-            <div>
-              <div className="@flex @justify-between">
+    <>
+      <Head>
+        <title>{playerName} | Wars World</title>
+      </Head>
+
+      <div className="@flex @flex-col @justify-center @items-center @align-middle">
+        <div className="@w-[95vw] @m-4 @px-4">
+          <section className="@h-full @bg-black/60 @mt-4 @rounded-t-xl @overflow-hidden">
+            <div className="@h-4 @w-full @bg-blue-moon" />
+            <div className="@flex @space-x-12 @px-12 @py-10">
+              <div className="@min-w-48 @h-48 @border-blue-moon @bg-black/50 @border-4 @text-center @overflow-hidden">
+                <img src="\img\CO\smoothFull\Awds-grit.webp" alt="grit" />
+              </div>
+              <div className="@min-h-48 @flex @flex-col">
                 <div>
-                  <div className="@flex @space-x-2">
-                    <img
-                      className="[image-rendering:pixelated] @self-center @w-8 @h-8"
-                      src="\img\nations\blue-moon.gif"
-                      alt="blue-moon"
+                  <div className="@flex @justify-between">
+                    <div>
+                      <div className="@flex @space-x-2">
+                        <img
+                          className="[image-rendering:pixelated] @self-center @w-8 @h-8"
+                          src="\img\nations\blue-moon.gif"
+                          alt="blue-moon"
+                        />
+                        <div className="@text-4xl @font-semibold">{playerName}</div>
+                      </div>
+                      <div className="@text-gray-500">Real Name</div>
+                    </div>
+                    <div className="@space-y-2">
+                      <div className="@flex @h-6 @space-x-2 @items-center">
+                        <div className="@bg-green-earth @h-6 @w-6 @rounded-full"></div>
+                        <div className="@text-xl">Online</div>
+                      </div>
+                      <div className="@text-base @text-gray-500">
+                        Last Activity: 05/21/2024 05:04pm
+                      </div>
+                    </div>
+                  </div>
+                  <div className="@pt-6 @text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, sed
+                    recusandae, perspiciatis libero minima porro ut quisquam alias vero ratione
+                    reiciendis optio voluptates totam dolor soluta enim repellendus asperiores
+                    voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+                    ipsam consequatur excepturi accusantium quos, eum et?
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <div className="@flex @space-x-4">
+            <div className="@col-span-6 @h-full @w-[75%]">
+              {/* Show general league stats */}
+              <section className="@grid @grid-cols-3 @gap-4 @py-8 @px-12 @h-full @bg-black/60 @my-4">
+                <PlayerMMRCard leagueType="Standard" rank={4} data={mmr_std_data} />
+                <PlayerMMRCard leagueType="Fog" rank={23} data={mmr_fog_data} />
+                <PlayerMMRCard leagueType="High Funds" rank={1456} data={mmr_hf_data} />
+                <PlayerMMRCard leagueType="Live Standard" rank={4} data={mmr_std_data} />
+                <PlayerMMRCard leagueType="Live Fog" rank={23} data={mmr_fog_data} />
+                <PlayerMMRCard leagueType="Live High Funds" rank={1456} data={mmr_hf_data} />
+              </section>
+              {/* Fully shows stats for one league */}
+              <section className="@pb-8 @px-12 @h-full @w-full @bg-black/60 @my-4 @space-y-4">
+                <div className="@grid @grid-cols-4">
+                  <Select
+                    className="@col-span-1 @self-center @h-12"
+                    options={gamemodes}
+                    onChange={setGamemode}
+                    value={gamemode}
+                  />
+                  <h1 className="@col-span-3 @font-russoOne @px-8">LIVE STANDARD</h1>
+                </div>
+                <div className="@grid @grid-cols-12 @gap-8">
+                  <div className="@flex @flex-col @col-span-3">
+                    <h2 className="@font-russoOne @text-5xl @my-2">Rank: #1</h2>
+                    <p className="@font-russoOne @text-2xl">MMR: 2784</p>
+                    <p className="@font-russoOne @text-2xl">Max MMR: 3342</p>
+                    <p className="@text-xl">Last game: 06/29/2023</p>
+                    <br />
+                    <div className="@flex @flex-col @py-4 @items-center @justify-center @align-middle @w-60">
+                      <MMRDataTable table={table} />
+                    </div>
+                  </div>
+                  <div className="@flex @flex-col @h-full @p-2 @col-span-5">
+                    <div className="@w-full @h-full @border-primary @border-4 @bg-bg-secondary @text-center">
+                      GRAPH
+                    </div>
+                  </div>
+                  <div className="@grid @grid-rows-5 @gap-2 @h-full @col-span-4">
+                    <SmallMatchCard
+                      matchResult="W"
+                      player1={{ coId: { name: "grimm", version: "AWDS" }, name: "Grimm Guy" }}
+                      player2={{ coId: { name: "nell", version: "AWDS" }, name: "Itou Kaiji" }}
+                      matchLink="/"
                     />
-                    <div className="@text-4xl @font-semibold">{playerName}</div>
+                    <SmallMatchCard
+                      matchResult="L"
+                      player1={{ coId: { name: "sonja", version: "AWDS" }, name: "Itou Kaiji" }}
+                      player2={{ coId: { name: "grimm", version: "AWDS" }, name: "Grimm Guy" }}
+                      matchLink="/"
+                    />
+                    <SmallMatchCard
+                      matchResult="D"
+                      player1={{ coId: { name: "sasha", version: "AWDS" }, name: "CliveGlitch" }}
+                      player2={{ coId: { name: "javier", version: "AWDS" }, name: "Itou Kaiji" }}
+                      matchLink="/"
+                    />
+                    <SmallMatchCard
+                      matchResult="W"
+                      player1={{ coId: { name: "sonja", version: "AWDS" }, name: "Itou Kaiji" }}
+                      player2={{ coId: { name: "grimm", version: "AWDS" }, name: "Grimm Guy" }}
+                      matchLink="/"
+                    />
+                    <SmallMatchCard
+                      matchResult="W"
+                      player1={{ coId: { name: "grimm", version: "AWDS" }, name: "Grimm Guy" }}
+                      player2={{ coId: { name: "olaf", version: "AWDS" }, name: "Itou Kaiji" }}
+                      matchLink="/"
+                    />
                   </div>
-                  <div className="@text-gray-500">Real Name</div>
                 </div>
-                <div className="@space-y-2">
-                  <div className="@flex @h-6 @space-x-2 @items-center">
-                    <div className="@bg-green-earth @h-6 @w-6 @rounded-full"></div>
-                    <div className="@text-xl">Online</div>
+              </section>
+              <section className="@pb-8 @px-12 @h-full @w-full @bg-black/60 @my-4 @space-y-2">
+                <h1 className="@col-span-3 @text-center @font-russoOne">Favorite Games</h1>
+                <div className="@grid @grid-cols-4 @h-48 @gap-4">
+                  <div className="@w-full @h-full @border-primary @border-4 @bg-bg-secondary">
+                    <p className="@text-center">GAME</p>
                   </div>
-                  <div className="@text-base @text-gray-500">Last Activity: 05/21/2024 05:04pm</div>
+                  <div className="@w-full @h-full @border-primary @border-4 @bg-bg-secondary">
+                    <p className="@text-center">GAME</p>
+                  </div>
+                  <div className="@w-full @h-full @border-primary @border-4 @bg-bg-secondary">
+                    <p className="@text-center">GAME</p>
+                  </div>
+                  <div className="@w-full @h-full @border-primary @border-4 @bg-bg-secondary">
+                    <p className="@text-center">GAME</p>
+                  </div>
                 </div>
-              </div>
-              <div className="@pt-6 @text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, sed recusandae,
-                perspiciatis libero minima porro ut quisquam alias vero ratione reiciendis optio
-                voluptates totam dolor soluta enim repellendus asperiores voluptatum. Lorem ipsum
-                dolor sit amet, consectetur adipisicing elit. Provident ipsam consequatur excepturi
-                accusantium quos, eum et?
-              </div>
+              </section>
+            </div>
+            <div className="@h-full @w-[25%]">
+              <section className="@w-full @min-h-[56rem] @bg-black/60 @pb-8 @px-12 @my-4">
+                <div>Friends</div>
+              </section>
             </div>
           </div>
         </div>
-      </section>
-      {/* Show general league stats */}
-      <section className="@grid @grid-cols-3 @gap-4 @py-8 @px-12 @h-full @bg-black/60 @my-4">
-        <PlayerMMRCard leagueType="Standard" rank={4} data={mmr_std_data} />
-        <PlayerMMRCard leagueType="Fog" rank={23} data={mmr_fog_data} />
-        <PlayerMMRCard leagueType="High Funds" rank={1456} data={mmr_hf_data} />
-        <PlayerMMRCard leagueType="Live Standard" rank={4} data={mmr_std_data} />
-        <PlayerMMRCard leagueType="Live Fog" rank={23} data={mmr_fog_data} />
-        <PlayerMMRCard leagueType="Live High Funds" rank={1456} data={mmr_hf_data} />
-      </section>
-      {/* Fully shows stats for one league */}
-      <section className="@pb-8 @px-12 @h-full @w-full @bg-black/60 @my-4 @space-y-4">
-        <div className="@grid @grid-cols-4">
-          <Select
-            className="@col-span-1 @self-center @h-12"
-            options={gamemodes}
-            onChange={setGamemode}
-            value={gamemode}
-          />
-          <h1 className="@col-span-3 @text-center @font-russoOne">LIVE STANDARD</h1>
-        </div>
-        <div className="@grid @grid-cols-12 @gap-8">
-          <div className="@flex @flex-col @col-span-3">
-            <h2 className="@font-russoOne @text-5xl @my-2">Rank: #1</h2>
-            <p className="@font-russoOne @text-2xl">MMR: 2784</p>
-            <p className="@font-russoOne @text-2xl">Max MMR: 3342</p>
-            <p className="@text-xl">Last game: 06/29/2023</p>
-            <br />
-            <div className="@flex @flex-col @py-4 @items-center @justify-center @align-middle @w-80">
-              <MMRDataTable table={table} />
-            </div>
-          </div>
-          <div className="@flex @flex-col @h-full @p-2 @col-span-5">
-            <div className="@w-full @h-full @border-primary @border-4 @bg-bg-secondary @text-center">
-              GRAPH
-            </div>
-          </div>
-          <div className="@grid @grid-rows-5 @gap-2 @h-full @col-span-4">
-            <SmallMatchCard
-              matchResult="W"
-              player1={{ coId: { name: "grimm", version: "AWDS" }, name: "Grimm Guy" }}
-              player2={{ coId: { name: "nell", version: "AWDS" }, name: "Itou Kaiji" }}
-              matchLink="/"
-            />
-            <SmallMatchCard
-              matchResult="L"
-              player1={{ coId: { name: "sonja", version: "AWDS" }, name: "Itou Kaiji" }}
-              player2={{ coId: { name: "grimm", version: "AWDS" }, name: "Grimm Guy" }}
-              matchLink="/"
-            />
-            <SmallMatchCard
-              matchResult="D"
-              player1={{ coId: { name: "sasha", version: "AWDS" }, name: "CliveGlitch" }}
-              player2={{ coId: { name: "javier", version: "AWDS" }, name: "Itou Kaiji" }}
-              matchLink="/"
-            />
-            <SmallMatchCard
-              matchResult="W"
-              player1={{ coId: { name: "sonja", version: "AWDS" }, name: "Itou Kaiji" }}
-              player2={{ coId: { name: "grimm", version: "AWDS" }, name: "Grimm Guy" }}
-              matchLink="/"
-            />
-            <SmallMatchCard
-              matchResult="W"
-              player1={{ coId: { name: "grimm", version: "AWDS" }, name: "Grimm Guy" }}
-              player2={{ coId: { name: "olaf", version: "AWDS" }, name: "Itou Kaiji" }}
-              matchLink="/"
-            />
-          </div>
-        </div>
-      </section>
-      <section className="@pb-8 @px-12 @h-full @w-full @bg-black/60 @my-4 @space-y-2">
-        <h1 className="@col-span-3 @text-center @font-russoOne">Favorite Games</h1>
-        <div className="@grid @grid-cols-4 @h-64 @gap-4">
-          <div className="@w-full @h-full @border-primary @border-4 @bg-bg-tertiary">
-            <p className="@text-center">GAME</p>
-          </div>
-          <div className="@w-full @h-full @border-primary @border-4 @bg-bg-tertiary">
-            <p className="@text-center">GAME</p>
-          </div>
-          <div className="@w-full @h-full @border-primary @border-4 @bg-bg-tertiary">
-            <p className="@text-center">GAME</p>
-          </div>
-          <div className="@w-full @h-full @border-primary @border-4 @bg-bg-tertiary">
-            <p className="@text-center">GAME</p>
-          </div>
-        </div>
-      </section>
-    </div>
+      </div>
+    </>
   );
 }
