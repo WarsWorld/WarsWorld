8e7b7a66dc382d8565320bce2b43ada079d09557
Enrique Chávez <56766746+CliveGlitch@users.noreply.github.com>
2024-10-28 11:30:56 -0600

    refac: SSR for Blogs

--- src/pages/articles/[...slug].tsx
+++ src/pages/articles/[...slug].tsx
@@ -1,27 +1,43 @@
 import Article from "frontend/components/layout/article/Article";
 import { markdownToHTML, stringToSlug } from "frontend/utils/articleUtils";
 import { trpc } from "frontend/utils/trpc-client";
-import type { GetServerSideProps } from "next";
+import type { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { prisma } from "server/prisma/prisma-client";
 
-type Props = {
-  articleId: string;
-  title: string;
-};
+export const getStaticPaths = (async () => {
+  const articles = await prisma.article.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  });
+  const paths = articles?.map((article) => {
+    return {
+      params: {
+        slug: [article.id.toString(), stringToSlug(article.title)],
+      },
+    };
+  });
 
-export const getServerSideProps: GetServerSideProps<Props> = async ({ params }) => {
-  // These params are the ones you put on the url
-  const articleId = params?.slug?.[0];
-  const paramSlug = params?.slug?.[1];
+  return {
+    paths: paths ?? [],
+    fallback: true,
+  };
+}) satisfies GetStaticPaths;
 
-  // If the url Id is invalid
-  if (articleId == undefined || !/^[0-9]+$/.test(articleId)) {
+export const getStaticProps = async (context: GetStaticPropsContext<{ slug: string[] }>) => {
+  // Url Params
+  const articleId = context.params?.slug[0];
+  const paramSlug = context.params?.slug[1];
+
+  const isArticleIdInt = articleId == undefined || !/^[0-9]+$/.test(articleId);
+
+  if (isArticleIdInt) {
     return {
       notFound: true,
     };
   }
-
-  // Get article title to compare it with the one that's on the url
+  // Get article title name
   const article = await prisma.article.findFirst({
     select: {
       title: true,
@@ -31,18 +47,14 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ params }) 
     },
   });
 
-  // No article found
   if (article == undefined) {
     return {
       notFound: true,
     };
   }
-
   const title = stringToSlug(article.title);
-
-  // If the slug is incorrect or there are more params
   // Redirect to correct slug
-  if (title != paramSlug || params?.slug?.[2] != undefined) {
+  if (title != paramSlug || context.params?.slug[2] != undefined) {
     return {
       redirect: {
         destination: `/articles/${articleId}/${title}`,
@@ -50,17 +62,17 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ params }) 
       },
     };
   }
-
   // Render the page
   return {
     props: {
-      articleId: articleId,
+      articleId: articleId ?? "",
       title: title,
     },
+    revalidate: 60,
   };
 };
 
-export default function NewsArticle({ articleId }: Props) {
+export default function ArticlePage({ articleId }: InferGetStaticPropsType<typeof getStaticProps>) {
   const { data: articleData } = trpc.article.getMarkdownById.useQuery(
     { id: articleId },
     { enabled: articleId != undefined },
