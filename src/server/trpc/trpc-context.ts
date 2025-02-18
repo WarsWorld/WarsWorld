3eb7370e4c740819d1aaf750b82b3249c7c5a3b3
FemboyDeveloper <j1a1b1m1@Gmail.com>
2025-02-17 20:59:00 -0500

    Current setup

--- src/server/trpc/trpc-context.ts
+++ src/server/trpc/trpc-context.ts
@@ -1,16 +1,16 @@
-/**
- * Creates context for an incoming request
- * @link https://trpc.io/docs/context
- */
 import type { CreateWSSContextFnOptions } from "@trpc/server/adapters/ws";
 import type { CreateNextContextOptions } from "@trpc/server/src/adapters/next";
 import { getSession } from "next-auth/react";
 
 export async function createContext(opts: CreateNextContextOptions | CreateWSSContextFnOptions) {
-  const session = await getSession({ req: opts.req });
-  //console.log("createContext for", session?.user?.name ?? "unknown user");
+  const req = "req" in opts ? opts.req : undefined;
+  const session = await getSession({ req });
+
   return {
     session,
+    req,
+    // Include res for Next.js API routes
+    res: "res" in opts ? opts.res : undefined,
   };
 }
 
