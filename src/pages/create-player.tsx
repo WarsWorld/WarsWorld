e30769d55282adec48379de6cb8017f463f139e7
cliveG <56766746+CliveGlitch@users.noreply.github.com>
2025-02-23 20:28:21 -0600

    text

--- src/pages/create-player.tsx
+++ src/pages/create-player.tsx
@@ -68,7 +68,7 @@ export default function CreatePlayer() {
       )}
       <h1>WORK IN PROGRESS</h1>
       <p>
-        It's for this page functionality to be moved to user's dropdown pop up on the top right.
+        This page will be removed in the future, it will be moved to the top right pop up later.
       </p>
       <p>
         If you get an error that ends with &quot;Unique constraint failed on the fields:
